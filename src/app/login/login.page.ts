19277fea59916610bfc547f562607a803e3fbd5c
Manuel Gonzalez <36157180+genma2612@users.noreply.github.com>
2022-10-11 22:16:35 -0300

    Initial commit

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  validations_form: FormGroup;
+  errorMessage: string = '';
+
+  validation_messages = {
+   'username': [
+     { type: 'required', message: 'username is required.' },
+     
+   ],
+   'password': [
+     { type: 'required', message: 'Password is required.' },
+     { type: 'minlength', message: 'Password must be at least 5 characters long.' }
+   ]
+ };
+
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.validations_form = this.formBuilder.group({
+      username: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(5),
+        Validators.required
+      ])),
+    });
+  }
+
+  tryLogin(value){
+    this.authService.doLogin(value)
+    .then(res => {
+      this.router.navigate(["/home"]);
+    }, err => {
+      this.errorMessage = err.message;
+      console.log(err)
+    })
+  }
+
+  goRegisterPage(){
+    this.router.navigate(["/register"]);
+  }
+}
